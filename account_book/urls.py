71f0cfaf8a81fb8cf244bc67c53f1c3fe0c505ca
Beam-ki <skunn123@gmail.com>
2023-01-04 08:03:57 +0900

    feat : 가계부 페이징화 성공

--- account_book/urls.py
+++ account_book/urls.py
@@ -5,5 +5,6 @@ from account_book import views
 urlpatterns=[
     path('create/',views.account_bookCreateView.as_view()),
     path('view/',views.account_bookView.as_view()),
+    path('accout_book_view/',views.accout_book_view.as_view())
 
 ]
--- account_book/views.py
+++ account_book/views.py
@@ -4,9 +4,17 @@ from account_book.serializers import account_bookSerializer,account_book_Create_
 from .models import account_book
 from rest_framework.response import Response
 from rest_framework import status, permissions
+from .pagination import PageNumberPagination, get_pagination_result
 from rest_framework.generics import get_object_or_404
 # Create your views here.
-
+class accout_book_view(APIView):
+    def get(self, request):
+        account_books = account_book.objects.filter().order_by('-created_at')
+        paginator = PageNumberPagination()
+        p = paginator.paginate_queryset(queryset=account_books, request=request)
+        paging = get_pagination_result(paginator, account_books.count())
+        serializer = account_bookSerializer(p, many=True)
+        return Response({"data": serializer.data, "page":paging}, status=status.HTTP_200_OK)
 class account_bookCreateView(APIView):
     permission_classes=[permissions.IsAuthenticated]
     def post(self, request):
@@ -16,11 +24,11 @@ class account_bookCreateView(APIView):
             return Response({"data": serializer.data, "message": "생성이 완료되었습니다"}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 
 class account_bookView(APIView):
     
-
     def get(self, request):
         account_book_id = int(request.GET.get('account_book_id', None))
         Account_book = get_object_or_404(account_book, id=account_book_id)
