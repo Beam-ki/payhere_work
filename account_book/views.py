6929a265e69670e7bdb8831dba94092e6f995758
Beam-ki <skunn123@gmail.com>
2023-01-04 07:13:06 +0900

    feat :로그인,회원가입,글작성 수정 삭제 조회

--- account_book/views.py
+++ account_book/views.py
@@ -9,34 +9,35 @@ from rest_framework.generics import get_object_or_404
 
 class account_bookCreateView(APIView):
     permission_classes=[permissions.IsAuthenticated]
-
     def post(self, request):
         serializer = account_book_Create_Serializer(data=request.data)
-        print(request.user)
         if serializer.is_valid(raise_exception=True):
             serializer.save(user=request.user)
             return Response({"data": serializer.data, "message": "생성이 완료되었습니다"}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class account_bookView(APIView):
+    
 
     def get(self, request):
         account_book_id = int(request.GET.get('account_book_id', None))
         Account_book = get_object_or_404(account_book, id=account_book_id)
         serializer = account_book_Create_Serializer(Account_book)
-        return Response({"products":serializer.data}, status=status.HTTP_200_OK)
+        return Response({"products":serializer.data}, status=status.HTTP_200_OK)   
     
     def put(self, request):
-        account_book_id = int(request.GET.get('account_book_id', None))
-        Account_book = get_object_or_404(account_book, id=account_book_id)
-        serializer = account_bookSerializer(Account_book, data= request.data, partial = True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message":"수정되었습니다.", "data":serializer.data}, status=status.HTTP_200_OK)
-        else:
-            return Response({"message":serializer.error})
-
+            account_book_id = int(request.GET.get('account_book_id', None))
+            Account_book = get_object_or_404(account_book, id=account_book_id)
+            serializer = account_bookSerializer(Account_book, data= request.data, partial = True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"message":"수정되었습니다.", "data":serializer.data}, status=status.HTTP_200_OK)
+            else:
+                return Response({"message":serializer.error})
+            
+    
     def delete(self, request):
         account_book_id = int(request.GET.get('account_book_id', None))
         Account_book = get_object_or_404(account_book, id=account_book_id)
